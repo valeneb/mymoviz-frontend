a3ab40507d2f85ba9d991f2f9a2c7b81d3461b8c
Valène Bonnet <bonnet.valene@gmail.com>
2023-09-27 18:02:54 +0200

    m

--- components/Home.js
+++ components/Home.js
@@ -46,7 +46,7 @@ function Home() {
   // ];
 
   useEffect(() => {
-    fetch("http://localhost:3000/movies")
+    fetch("https://mymoviz-backend-ldtd.vercel.app/movies")
       .then((response) => response.json())
       .then((data) => {
         setMoviesData(data.movies);
--- script.js
+++
@@ -1,5 +0,0 @@
-fetch("https://mymoviz-backend-livid.vercel.app/")
-  .then((response) => response.json())
-  .then((data) => {
-    document.querySelector("#msg").textContent = data.message;
-  });
